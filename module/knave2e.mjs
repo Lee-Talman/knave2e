6fed21c93ea285c6556b2f0c60e3654a33442cdc
Lee Talman <leetlmn@gmail.com>
2024-03-22 21:25:58 -0400

    add game settings for arrows/bullets/coins per slot, calculating level from xp, xp-to-level-2, max wounds, and armor stacking. Integrated calculateLevel setting.

--- module/knave2e.mjs
+++ module/knave2e.mjs
@@ -26,19 +26,20 @@ Hooks.on("init", () => {
 
 Hooks.once("init", () => {
   CONFIG.SYSTEM = SYSTEM;
+
 });
 
-Hooks.once("i18nInit", function() {
+Hooks.once("i18nInit", function () {
 
   // Apply localizations
   const toLocalize = [
     "ABILITIES", "AMMO", "ARMOR", "COIN", "EQUIPMENT", "SPELLBOOK", "WEAPON"];
-  for ( let c of toLocalize ) {
+  for (let c of toLocalize) {
     const conf = foundry.utils.getProperty(SYSTEM, c);
-    for ( let [k, v] of Object.entries(conf) ) {
-      if ( v.label ) v.label = game.i18n.localize(v.label);
-      if ( v.abbreviation) v.abbreviation = game.i18n.localize(v.abbreviation);
-      if ( typeof v === "string" ) conf[k] = game.i18n.localize(v);
+    for (let [k, v] of Object.entries(conf)) {
+      if (v.label) v.label = game.i18n.localize(v.label);
+      if (v.abbreviation) v.abbreviation = game.i18n.localize(v.abbreviation);
+      if (typeof v === "string") conf[k] = game.i18n.localize(v);
     }
   }
 });
@@ -53,6 +54,105 @@ Hooks.once('init', function () {
     Knave2eChatMessage,
     rollItemMacro
   };
+
+  // Set globals from game settings (TODO: localize)
+
+  // Armor Restriction
+  game.settings.register('knave2e', 'restrictArmor', {
+    name: "Enforce Armor Restriction",
+    hint: "Characters cannot equip multiple armor pieces of the same type. Defaults to TRUE.",
+    scope: "world",
+    config: true,
+    type: Boolean,
+    default: true,
+    requiresReload: true,
+  });
+
+  // Calculate Level
+  game.settings.register('knave2e', 'calculateLevel', {
+    name: "Calculate Level",
+    hint: "Automatically calculate level from XP. Defaults to TRUE.",
+    scope: "world",
+    config: true,
+    type: Boolean,
+    default: true,
+    requiresReload: true,
+  });
+
+  // Calculate Wounds
+  game.settings.register('knave2e', 'calculateWounds', {
+    name: "Calculate Maximum Wounds",
+    hint: "Automatically calculate maximum wounds as 10 + CON. Defaults to TRUE.",
+    scope: "world",
+    config: true,
+    type: Boolean,
+    default: true,
+    requiresReload: true,
+  });
+
+  // XP to Level 2
+  game.settings.register('knave2e', 'baseLevelXP', {
+    name: "Change XP to Level 2",
+    hint: "Set XP required to reach level 2. Each subsequent level requires approximately twice as much XP. Defaults to 2000.",
+    scope: "world",
+    config: true,
+    type: Number,
+    default: 2000,
+    range: {
+      min: 1,
+      step: 1,
+      max: 10000,
+    },
+    requiresReload: true,
+  });
+
+  // Arrows Per Slot
+  game.settings.register('knave2e', 'arrowsPerSlot', {
+    name: "Arrows Per Slot",
+    hint: "Set to 0 to disable automatic slot tracking for arrows. Defaults to 20.",
+    scope: "world",
+    config: true,
+    type: Number,
+    default: 20,
+    requiresReload: true,
+    range: {
+      min: 0,
+      step: 1,
+      max: 1000
+    }
+  });
+
+  // Sling Bullets Per Slot
+  game.settings.register('knave2e', 'slingBulletsPerSlot', {
+    name: "Sling Bullets Per Slot",
+    hint: "Set to 0 to disable automatic slot tracking for sling bullets. Defaults to 5.",
+    scope: "world",
+    config: true,
+    type: Number,
+    default: 5,
+    requiresReload: true,
+    range: {
+      min: 0,
+      step: 1,
+      max: 1000,
+    }
+  });
+
+  // Coins Per Slot
+  game.settings.register('knave2e', 'coinsPerSlot', {
+    name: "Coins Per Slot",
+    hint: "Set to 0 to disable automatic slot tracking for coins. Defaults to 500.",
+    scope: "world",
+    config: true,
+    type: Number,
+    default: 500,
+    requiresReload: true,
+    range: {
+      min: 0,
+      step: 1,
+      max: 10000,
+    }
+  });
 
   // globalThis.knave2e = game.system;
   // game.system.CONST = SYSTEM;
