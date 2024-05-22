902c233c4b6a34dcd35d80b521e899fff2038b38
Lee Talman <L.Talman@engusasecure.com>
2024-05-22 02:38:23 -0400

    refactor damage rolls into single user input

--- module/data/monster-attack.mjs
+++ module/data/monster-attack.mjs
@@ -14,7 +14,7 @@ export default class Knave2eMonsterAttack extends Knave2eItemType {
         schema.damageDiceAmount = new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 });
         schema.damageDiceSize = new fields.StringField({ required: false, choices: SYSTEM.DAMAGE_DICE_SIZES, initial: "d6" });
         schema.damageDiceBonus = new fields.NumberField({ required: true, nullable: false, integer: false, initial: 0, min: -999 });
-        // schema.damageRoll = new fields.StringField({required: true, initial: "1d6"});
+        schema.damageRoll = new fields.StringField({required: true, initial: "1d6"});
 
         return schema;
     }
@@ -23,17 +23,33 @@ export default class Knave2eMonsterAttack extends Knave2eItemType {
     }
 
     prepareDerivedData() {
-        let damageRoll = "";
-        if (this.damageDiceBonus > 0) {
-            damageRoll = `${this.damageDiceAmount}${this.damageDiceSize}+${this.damageDiceBonus}`;
+        const rollDamage = new RegExp("^(\\d*)(d[1-9]\\d*)(?:([+-])(\\d+))?$", "");
+        const flatDamage = new RegExp("^(\\+?|\\-?)(\\d+)$", "");
+        let m;
+    
+        if ((m = rollDamage.exec(this.damageRoll)) !== null) {
+          m.forEach((match, groupIndex) => {
+            console.log(`Found match, group ${groupIndex}: ${match}`);
+          });
+    
+          this.damageDiceAmount = m[1] === "" ? 1 : m[1];
+          this.damageDiceSize = m[2];
+    
+          if (m[3] !== "" && m[3] !== undefined && m[3] !== null) {
+            this.damageDiceBonus = m[3] === "+" ? +m[4] : +`-${m[4]}`;
+          } else {
+            this.damageDiceBonus = 0;
+          }
+        } else if ((m = flatDamage.exec(this.damageRoll)) !== null) {
+          this.damageDiceAmount = 0;
+          this.damageDiceSize = "d6";
+    
+          if (m[1] !== "" && m[1] !== undefined && m[1] !== null) {
+            this.damageDiceBonus = m[1] === "-" ? +`-${m[2]}` : +m[2];
+          } else {
+            this.damageDiceBonus = +m[2];
+          }
         }
-        else if (this.damageDiceBonus === 0) {
-            damageRoll = `${this.damageDiceAmount}${this.damageDiceSize}`;
-        }
-        else {
-            damageRoll = `${this.damageDiceAmount}${this.damageDiceSize}${this.damageDiceBonus}`;
-        }
-        this.damageRoll = damageRoll;
     }
 }
 
