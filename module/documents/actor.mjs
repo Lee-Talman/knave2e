797d608d37a599a149628805b707d5713765340b
Lee-Talman <leetlmn@gmail.com>
2025-05-17 13:52:10 -0400

    enable vision automatically for new character tokens

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -1,9 +1,12 @@
 export default class Knave2eActor extends Actor {
   async _preCreate(data) {
+    if ((await super._preCreate(data)) === false) return false;
     if (data.type === "character" || data.type === "recruit") {
       this.updateSource({ "prototypeToken.actorLink": true });
     }
-    super._preCreate(data);
+    if (data.type === "character") {
+      this.updateSource({ "prototypeToken.sight.enabled": true });
+    }
   }
 
   prepareData() {
@@ -116,5 +119,4 @@ export default class Knave2eActor extends Actor {
 
     // Process additional Vehicle data here.
   }
-
 }
